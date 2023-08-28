3e5c5f7c646a42204cf376b31cde4dcd533ff91f
mpascualach <miguelpacheson@hotmail.es>
2023-08-28 18:17:10 +0200

    working with new font

--- src/components/BlockComponent.js
+++ src/components/BlockComponent.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import BlockInterface from "../interfaces/BlockInterface";
+import { utils } from "ethers";
 
 const Block = ({ block }) => {
   // useEffect(() => {
@@ -20,13 +20,19 @@ const Block = ({ block }) => {
     return formattedDate;
   };
 
+  const calculateAvgValue = (block) => {
+    const totalValue = block.transactions.reduce((a, b) => {
+      return a + b.value;
+    });
+  };
+
   return (
     <div className="block-details p-5 w-full bg-[#B27E41] rounded-[10px] flex justify-between text-white text-xl/[20px] shadow-md">
       <div
         className="flex flex-col items-start gap-[20px]"
         style={{ fontSize: "24px" }}
       >
-        <p>#{block.number}</p>
+        <p>Block #{block.number}</p>
         <p style={{ fontSize: "18px" }}>{convertTimestamp(block.timestamp)}</p>
       </div>
       <div className="flex flex-col items-start gap-[20px]">
